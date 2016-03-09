d1811ce8d02be97ae434c1ff97ed5a5d14f3512f
Romuald PERROT <perrot.romuald@gmail.com>
2016-03-09 12:38:30 +0100

    - Add license

--- src/openMVG/graph/graph_connected_component.hpp
+++ src/openMVG/graph/graph_connected_component.hpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2016 Romuald PERROT.
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #ifndef _OPENMVG_GRAPH_GRAPH_CONNECTED_COMPONENT_HPP_
 #define _OPENMVG_GRAPH_GRAPH_CONNECTED_COMPONENT_HPP_
 
