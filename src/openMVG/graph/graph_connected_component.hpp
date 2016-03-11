cd4d72e7bdf5f30aa5d5e7f66f5c78eb84b978c6
Romuald PERROT <perrot.romuald@gmail.com>
2016-03-11 12:26:25 +0100

    - Get connected component size - Get largest connected component

--- src/openMVG/graph/graph_connected_component.hpp
+++ src/openMVG/graph/graph_connected_component.hpp
@@ -24,11 +24,28 @@ struct GraphConnectedComponents
 
     std::vector< node_type * > GetCC( const GraphType & g ) const ;
 
+    /**
+    * @brief Get number of node in each connected components
+    */
+    std::vector< size_t > GetCCNodeCount( const GraphType & g ) const ;
+
+    /**
+    * @brief Get largest CC
+    * @return a representant of the largest CC
+    */
+    node_type * GetLargestCC( const GraphType & g ) const ;
+
+    /**
+    * @brief Get a copy of each connected components
+    * @return a copy of each CC
+    */
+    std::vector< GraphType > GetCCCopy( const GraphType & g ) const ;
+
 
   private:
 
     void GetCC( const GraphType & g , node_type * from_node , std::map<node_type*, bool> & used ) const ;
-
+    size_t GetCCNodeCount( const GraphType & g , node_type * from_node , std::map<node_type*, bool> & used ) const ;
 } ;
 
 template< typename GraphType >
@@ -84,6 +101,123 @@ void GraphConnectedComponents<GraphType>::GetCC( const GraphType & g ,
     }
   }
 }
+
+template< typename GraphType >
+size_t GraphConnectedComponents<GraphType>::GetCCNodeCount( const GraphType & g , typename GraphConnectedComponents<GraphType>::node_type * from_node , std::map<typename GraphConnectedComponents<GraphType>::node_type*, bool> & used ) const
+{
+  used[ from_node ] = true ;
+
+  size_t cur_size = 1 ;
+
+  auto & neighs = from_node->Neighbors() ;
+
+  for( auto it_neigh = neighs.begin() ; it_neigh != neighs.end() ; ++it_neigh )
+  {
+    const edge_type * cur_edge = *it_neigh ;
+    node_type * opp = cur_edge->Opposite( from_node ) ;
+
+    if( ! used.count( opp ) )
+    {
+      cur_size += GetCCNodeCount( g , opp , used ) ;
+    }
+  }
+  return cur_size ;
+}
+
+
+/**
+* @brief Get number of node in each connected components
+*/
+template< typename GraphType >
+std::vector< size_t > GraphConnectedComponents<GraphType>::GetCCNodeCount( const GraphType & g ) const
+{
+  const std::vector< node_type * > & nodes = g.Nodes() ;
+  std::vector< size_t > res ;
+
+  int nb_left = nodes.size() ;
+
+  if( nodes.size() > 0 )
+  {
+    std::map<node_type *, bool> used ;
+
+    // Loop until some nodes haven't been found
+    while( nb_left > 0 )
+    {
+      // Look for an unused node and start from it
+      int id = 0 ;
+      while( used.count( nodes[id] ) != 0 )
+      {
+        ++id ;
+      }
+
+      // Computes CC from this node and returns it's size
+      res.push_back( GetCCNodeCount( g , nodes[id] , used ) ) ;
+
+      // Update nb unseen nodes
+      nb_left = nodes.size() - used.size() ;
+    }
+  }
+
+  return res ;
+}
+
+/**
+* @brief Get largest CC
+* @return a representant of the largest CC
+*/
+template< typename GraphType >
+typename GraphConnectedComponents<GraphType>::node_type * GraphConnectedComponents<GraphType>::GetLargestCC( const GraphType & g ) const
+{
+  const std::vector< node_type * > & nodes = g.Nodes() ;
+  node_type * res = nullptr ;
+  size_t cur_max_cc_size = 0 ;
+
+
+  int nb_left = nodes.size() ;
+
+  if( nodes.size() > 0 )
+  {
+    std::map<node_type *, bool> used ;
+
+    // Loop until some nodes haven't been found
+    while( nb_left > 0 )
+    {
+      // Look for an unused node and start from it
+      int id = 0 ;
+      while( used.count( nodes[id] ) != 0 )
+      {
+        ++id ;
+      }
+
+      // Computes CC from this node and returns it's size
+      const size_t cc_size = GetCCNodeCount( g , nodes[id] , used ) ;
+
+      // Check if it's a bigger CC
+      if( cc_size > cur_max_cc_size )
+      {
+        cur_max_cc_size = cc_size ;
+        res = nodes[ id ] ;
+      }
+
+      // Update nb unseen nodes
+      nb_left = nodes.size() - used.size() ;
+    }
+  }
+
+  return res ;
+
+}
+
+/**
+* @brief Get a copy of each connected components
+* @return a copy of each CC
+*/
+template< typename GraphType >
+std::vector< GraphType > GraphConnectedComponents<GraphType>::GetCCCopy( const GraphType & g ) const
+{
+
+}
+
 
 
 }
--- src/openMVG/graph/graph_connected_component_test.cpp
+++ src/openMVG/graph/graph_connected_component_test.cpp
@@ -57,7 +57,62 @@ TEST( graphConnectedComponent , TestCC )
   EXPECT_EQ( 5 , g.NbNode() ) ;
   EXPECT_EQ( 5 , g.NbEdge() ) ;
   EXPECT_EQ( 1 , cc.size() ) ; // (n1,n2,n3,n4,n5)
+}
 
+TEST( graphConnectedComponent , connectedComponentsSize )
+{
+  UndirectedGraph<> g ;
+
+  UndirectedGraph<>::node_type * n1 = g.AddNode() ;
+  UndirectedGraph<>::node_type * n2 = g.AddNode() ;
+  UndirectedGraph<>::node_type * n3 = g.AddNode() ;
+  UndirectedGraph<>::node_type * n4 = g.AddNode() ;
+  UndirectedGraph<>::node_type * n5 = g.AddNode() ;
+
+  GraphConnectedComponents< UndirectedGraph<> > cc_solver ;
+
+  std::vector< size_t > cc_count = cc_solver.GetCCNodeCount( g ) ;
+
+  EXPECT_EQ( 5 , cc_count.size() ) ;
+  EXPECT_EQ( 1 , cc_count[0] ) ;
+  EXPECT_EQ( 1 , cc_count[1] ) ;
+  EXPECT_EQ( 1 , cc_count[2] ) ;
+  EXPECT_EQ( 1 , cc_count[3] ) ;
+  EXPECT_EQ( 1 , cc_count[4] ) ;
+
+  UndirectedGraph<>::edge_type * e1 = g.AddEdge( n1 , n2 ) ;
+  UndirectedGraph<>::edge_type * e2 = g.AddEdge( n2 , n3 ) ;
+  UndirectedGraph<>::edge_type * e3 = g.AddEdge( n4 , n5 ) ;
+
+  cc_count = cc_solver.GetCCNodeCount( g ) ;
+
+  EXPECT_EQ( 2 , cc_count.size() ) ;
+  EXPECT_EQ( 5 , cc_count[0] + cc_count[1] ) ;
+
+  EXPECT_EQ( true , cc_count[0] == 3 || cc_count[0] == 2 ) ;
+  EXPECT_EQ( true , cc_count[1] == 2 || cc_count[1] == 3 ) ;
+  EXPECT_EQ( true , cc_count[0] != cc_count[1] ) ;
+}
+
+TEST( graphConnectedComponent , largestCC )
+{
+  UndirectedGraph<> g ;
+
+  UndirectedGraph<>::node_type * n1 = g.AddNode() ;
+  UndirectedGraph<>::node_type * n2 = g.AddNode() ;
+  UndirectedGraph<>::node_type * n3 = g.AddNode() ;
+  UndirectedGraph<>::node_type * n4 = g.AddNode() ;
+  UndirectedGraph<>::node_type * n5 = g.AddNode() ;
+
+  UndirectedGraph<>::edge_type * e1 = g.AddEdge( n1 , n2 ) ;
+  UndirectedGraph<>::edge_type * e2 = g.AddEdge( n2 , n3 ) ;
+  UndirectedGraph<>::edge_type * e3 = g.AddEdge( n4 , n5 ) ;
+
+  GraphConnectedComponents< UndirectedGraph<> > cc_solver ;
+
+  UndirectedGraph<>::node_type * ref_big_cc = cc_solver.GetLargestCC( g ) ;
+
+  EXPECT_EQ( true , n1 == ref_big_cc || n2 == ref_big_cc || n3 == ref_big_cc ) ;
 }
 
 /* ************************************************************************* */
