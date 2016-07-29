9771a9519c86e1b1a66e9d7d13324da856b134e6
Romuald Perrot <perrot.romuald@gmail.com>
2016-07-29 10:02:37 +0200

    - Fix licence

--- src/openMVG/graph/graph_IO.hpp
+++ src/openMVG/graph/graph_IO.hpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2016 Romuald PERROT.
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #ifndef OPENMVG_GRAPH_GRAPH_IO_HPP_
 #define OPENMVG_GRAPH_GRAPH_IO_HPP_
 
