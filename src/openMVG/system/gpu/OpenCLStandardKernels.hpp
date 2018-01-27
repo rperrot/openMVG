21f16bd7a4ff2577964bfb0019990b987bee27f1
Romuald Perrot <perrot.romuald@gmail.com>
2018-01-27 18:38:30 +0100

    - Update license

--- src/openMVG/system/gpu/OpenCLStandardKernels.hpp
+++ src/openMVG/system/gpu/OpenCLStandardKernels.hpp
@@ -1,3 +1,11 @@
+// This file is part of OpenMVG, an Open Multiple View Geometry C++ library.
+
+// Copyright (c) 2018 Romuald PERROT.
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #ifndef OPENMVG_SYSTEM_GPU_STANDARD_KERNELS_HPP
 #define OPENMVG_SYSTEM_GPU_STANDARD_KERNELS_HPP
 
